f8e1a243f2c7d3904dd687d58944219e5567055c
p40-alpha-miner fork <fork@example.com>
2026-06-11 21:04:12 -0600

    ﻿Build script fix for Linux: add -fPIC and -allow-unsupported-compiler

--- p40-pearl-gemm/packaging/build_capi.sh
+++ p40-pearl-gemm/packaging/build_capi.sh
@@ -26,9 +26,12 @@ SRC=(
   csrc/gemm/noise_gemm_sm61.cu
 )
 
+# -Xcompiler -fPIC is required for a Linux shared library; -allow-unsupported-
+# compiler keeps newer host GCC (e.g. 13.3 on Ubuntu 24.04) from being rejected.
 "$NVCC" -shared -o "$OUT" "${SRC[@]}" \
   -I csrc -I csrc/gemm -I csrc/blake3 -I csrc/tensor_hash -I "$CUTLASS_DIR" \
-  -std=c++20 --expt-relaxed-constexpr --expt-extended-lambda --use_fast_math \
-  -gencode arch=compute_61,code=sm_61 -O3 -DNDEBUG -DP40_NO_TORCH
+  -Xcompiler -fPIC -std=c++20 --expt-relaxed-constexpr --expt-extended-lambda \
+  --use_fast_math -gencode arch=compute_61,code=sm_61 -O3 -DNDEBUG -DP40_NO_TORCH \
+  -allow-unsupported-compiler ${EXTRA_NVCC_FLAGS:-}
 
 echo "built $OUT"
